fbd4489d2c0d74ef61bfa6b81276b53c42a736d3
user <user@qedge.com>
2016-09-30 13:42:30 +0530

    second

---
+++ demo.bat
@@ -0,0 +1,4 @@
+cd c:\janu\orangehrm
+java -cp c:\excel\orangehrm\jars\*;c:\excel\
+orangehrm\bin
+org.testng.TestNG c:\excel\orangehrm\testng.xml
---
+++ src/orangehrm/demo.java
@@ -0,0 +1,12 @@
+package orangehrm;
+
+import org.testng.annotations.Test;
+
+public class demo {
+	@Test
+	public void init()
+	{
+		System.out.println("hello world");
+	}
+
+}
